ca958928798396a808eb2f462fdf0d0ba6cdc8a6
goodnesskay <gtkbrain@gmail.com>
2017-06-08 23:56:45 +0100

    Refactored folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '../assets/templates/app.component.html',
+  styleUrls: ['../assets/css/app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  dayo: Hero = {
+    id: 1,
+    name: 'Goodness'
+  };
 }
